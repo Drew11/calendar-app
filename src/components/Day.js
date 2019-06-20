51ada5e4b7990fd0d43c8886b600f73893a38de4
Drew11 <drew.naumenko@gmail.com>
2019-06-20 19:16:43 +0300

    in progress

--- src/components/Day.js
+++ src/components/Day.js
@@ -3,24 +3,11 @@ import React from 'react';
 
 function Day({firstDayNumber,daysInMonth , onDayClick}) {
 
-    const mapping = [];
-
-    for(let i = 0; i < daysInMonth;i++){
-      mapping.push(i+1)
-    }
-
-    console.log(mapping);
-    const day = mapping.map((day, index)=>{
-       return  <td id={day}
-                  onClick={()=>onDayClick(index)}
-        >{
-            day
-
-        }
-        </td>
-    });
-
-    return<>{day}</>
+    return<td id
+                 onClick={()=>onDayClick()}
+            >
+           { "must be value...."}
+    </td>
 }
 
 export default Day;
