94bdef5578137663673c14f607f68e74e5168b1f
Drew11 <drew.naumenko@gmail.com>
2019-06-21 19:24:23 +0300

    Updates

---
+++ precache-manifest.3bdcaa16fff1d29a992161021c16022c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a86f35be39c60cab6ab8dbeb2f5c9d9a",
+    "url": "/calendar-app/index.html"
+  },
+  {
+    "revision": "784fd8162cbe8993d9d4",
+    "url": "/calendar-app/static/css/2.43d354e1.chunk.css"
+  },
+  {
+    "revision": "fd249986766001d34d00",
+    "url": "/calendar-app/static/css/main.282579a0.chunk.css"
+  },
+  {
+    "revision": "784fd8162cbe8993d9d4",
+    "url": "/calendar-app/static/js/2.b3112ce9.chunk.js"
+  },
+  {
+    "revision": "fd249986766001d34d00",
+    "url": "/calendar-app/static/js/main.6193eaab.chunk.js"
+  },
+  {
+    "revision": "ed7684dd8ce66205982a",
+    "url": "/calendar-app/static/js/runtime~main.1c49685a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/calendar-app/precache-manifest.7e7d61d6d27ab4bd872de11f795d5411.js"
+  "/calendar-app/precache-manifest.3bdcaa16fff1d29a992161021c16022c.js"
 );
 
 self.addEventListener('message', (event) => {
